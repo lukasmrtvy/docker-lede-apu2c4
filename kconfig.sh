c6f6335dd0f22fb7ce58b876328e165aa358bcf4
Lukas M <lukas.mrtvy@gmail.com>
2018-02-11 17:48:03 +0100

    Update kconfig.sh

--- kconfig.sh
+++ kconfig.sh
@@ -6,6 +6,5 @@ kernel_conf_path="./target/linux/x86/64/config-default"
  while read CMD; do
    prefix=`echo ${CMD} | awk -F "=" '{print $1}'` ;
    option=`echo ${CMD} | awk -F "=" '{print $2}'`;
-   #sed -i ./target/linux/x86/64/config-default -e "s@^$prefix=\(.*\)@$CMD@g" ;
    grep -q "^$prefix=" ${kernel_conf_path} && sed "s@^$prefix=\(.*\)@$prefix=$option@g" -i ${kernel_conf_path} || sed "$ a$prefix=$option" -i ${kernel_conf_path};
  done
